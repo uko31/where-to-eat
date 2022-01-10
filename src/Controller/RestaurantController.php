ec9c143a01c69acb4cd1a30eba9f25f1746a6ee5
uko31 <m.gregoriades@gmail.com>
2022-01-10 14:41:47 +0100

    Restaurant controller added

---
+++ src/Controller/RestaurantController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RestaurantController extends AbstractController
+{
+    #[Route(
+        '/restaurant', 
+        name: 'restaurant',
+        methods: ['GET', 'POST']
+    )]
+    public function index(): Response
+    {
+        return $this->render('restaurant/index.html.twig', [
+            'controller_name' => 'RestaurantController',
+        ]);
+    }
+}
