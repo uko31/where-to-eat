234af37c35745ca568246f351d5a3d81f948948c
uko31 <m.gregoriades@gmail.com>
2022-01-11 10:54:39 +0100

    ajout du form_theme dans le twig.yaml

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,17 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\UserType;
 
 class UserController extends AbstractController
 {
     #[Route('/register', name: 'user_register')]
-    public function register(): Response
+    public function register(Request $request): Response
     {
         $form = $this->createForm(UserType::class);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        $form->handleRequest($request);
+        //dump($form->get('cgu')->getData());
+
+        if ($form->isSubmitted() && $form->isValid() && $form->get('cgu')->getData()) {
+            $user = $form->getData();
+            dd($user);
             // todo registration process
         }
 
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type as Type;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +13,16 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')
-            ->add('password')
+            ->add('firstname', Type\TextType::class)
+            ->add('lastname', Type\TextType::class)
+            ->add('email', Type\EmailType::class)
+            ->add('password', Type\PasswordType::class)
+            ->add('cgu', Type\CheckboxType::class, [
+                'label' => '<b>Je m\'engage à manger proprement!</b>',
+                'label_html' => true,
+                'mapped' => false,
+            ])
+            ->add('register', Type\SubmitType::class)
         ;
     }
 
